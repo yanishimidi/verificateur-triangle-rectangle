ab4546527a3547d052b0cf8c505d4732e52590ae
yanishimidi <150398346+yanishimidi@users.noreply.github.com>
2024-09-27 22:39:37 +0200

    Add files via upload

---
+++ code triangle isocele.py
@@ -0,0 +1,13 @@
+def est_isocele(a,b,c): 
+    if a==b or a==c or b==c: 
+        return True 
+    else: 
+        return False 
+
+print (est_isocele(1,2,3))
+        
+    
+    
+    
+    
+    
